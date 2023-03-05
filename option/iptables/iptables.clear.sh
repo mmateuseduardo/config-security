520857a18a8fb25d372a68921d184a4260dfbbda
Hello, I am Mateus Eduardo :) <tequel_rp@hotmail.com>
2023-03-04 23:13:37 -0300

    v1.9

--- option/iptables/iptables.clear.sh
+++ option/iptables/iptables.clear.sh
@@ -24,7 +24,6 @@
 # High Intensty Colors Used:                                                   #
 # https://gist.github.com/JBlond/2fea43a3049b38287e5e9cefc87b2124#file-bash-colors-md #
 # =============================================================================#
-
 ### BEGIN INIT INFO
 # Provides:          firewall
 # Required-Start:    $all
